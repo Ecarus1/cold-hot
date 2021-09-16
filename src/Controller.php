c69f43297a5ce2309992f972846b9c1fae9edeb7
Ecarus1 <solodovnikov.andrey99@mail.ru>
2021-09-16 23:33:23 +0300

    Task02

---
+++ src/Controller.php
@@ -0,0 +1,9 @@
+<?php 
+	namespace Ecarus1\coldhot\Controller;
+    use function Ecarus1\coldhot\View\showGame;
+
+    function startGame() {
+        echo "Game started".PHP_EOL;
+        showGame();
+    }
+?> 
---
+++ src/View.php
@@ -0,0 +1,6 @@
+<?php 
+	namespace Ecarus1\coldhot\View;
+    function showGame() {
+        echo "Game shown";
+    }
+?> 
